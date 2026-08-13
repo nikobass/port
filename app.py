dda272ff0a0f5766831a5abc579738f768c66bd4
nikobass <49431842+nikobass@users.noreply.github.com>
2026-08-13 14:26:26 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1259,7 +1259,7 @@ def montant_investi_affichage(row: pd.Series, transactions: pd.DataFrame) -> flo
 # ---------------------------
 # App
 # ---------------------------
-st.set_page_config(page_title="Dashboard BW", page_icon="📈", layout="wide")
+st.set_page_config(page_title="Dashboard Perso", page_icon="📈", layout="wide")
 st.markdown(PREMIUM_CSS, unsafe_allow_html=True)
 
 with st.sidebar:
